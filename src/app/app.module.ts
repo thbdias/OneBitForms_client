3e528722d55d6fe51c0c442db14524c9e99b27cc
thbdias <thiagobalbino91@gmail.com>
2019-04-23 17:53:56 -0300

    gerando nossos components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,27 @@ import { AppComponent } from './app.component';
 import { MaterializeModule } from 'ng2-materialize';
 import { ColorPickerModule } from 'ngx-color-picker';
 import { ChartsModule } from 'ng2-charts';
-
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AnswersModule } from './answers/answers.module';
+import { FormsModule } from './forms/forms.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterializeModule.forRoot(),
     ColorPickerModule,
-    ChartsModule
+    ChartsModule,
+    FormsModule,
+    AnswersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
